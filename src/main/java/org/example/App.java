575285a5ddac75ad2001a7c40d806a1ec7043814
Abhinav Chaudhary <abhinav.chaudhary@tothenew.com>
2025-07-15 13:50:51 +0530

    Created executable jar and printed 'Hello World'

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,17 @@
 package org.example;
 
 import com.abhinav.utils.DoSomething;
+
 public class App {
     public static void main( String[] args ) {
 
+        // Q2
         // Creating object of my util class
-        DoSomething doSomething = new DoSomething();
+//        DoSomething doSomething = new DoSomething();
         // Calling method from inside my custom dependency
-        doSomething.printSomethingFromDependency();
+//        doSomething.printSomethingFromDependency();
+
+        // Q3
+        System.out.println("Hello World!");
     }
 }
