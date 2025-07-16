ce7d261191531d61553dd288b5488f409a4a350f
Abhinav Chaudhary <abhinav.chaudhary@tothenew.com>
2025-07-16 10:35:20 +0530

    Base Spring Project

---
+++ src/test/java/com/abhinav/dummydemo/DummyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinav.dummydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
