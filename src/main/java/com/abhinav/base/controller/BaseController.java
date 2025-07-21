8f61db7e026aae331e0af8d886205d9999b772e7
Abhinav Chaudhary <abhinav.chaudhary@tothenew.com>
2025-07-21 19:58:24 +0530

    Added a rest controller that returns 'Hello World' on localhost:8080/

---
+++ src/main/java/com/abhinav/base/controller/BaseController.java
@@ -0,0 +1,12 @@
+package com.abhinav.base.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class BaseController {
+    @GetMapping("")
+    public String getHelloWorld() {
+        return "Hello World";
+    }
+}
