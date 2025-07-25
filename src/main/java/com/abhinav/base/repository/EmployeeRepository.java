83a6e0b89b4ccb2a07529f1ae9dc4822271a91ac
Abhinav Chaudhary <abhinav.chaudhary@tothenew.com>
2025-07-25 20:10:51 +0530

    Added Employee Repository

---
+++ src/main/java/com/abhinav/base/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.abhinav.base.repository;
+
+import com.abhinav.base.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
