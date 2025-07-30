0565cdf42c8a54bf56b124d6162cb6e8360c3689
Abhinav Chaudhary <abhinav.chaudhary@tothenew.com>
2025-07-30 22:56:40 +0530

    Implemented OneToMany Bi directional mapping between Author and Book

--- src/main/java/com/abhinav/base/BaseApplication.java
+++ src/main/java/com/abhinav/base/BaseApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("com.abhinav.base.entityOneToManyUnidirection")
+@EntityScan("com.abhinav.base.entityOneToManyBiDirection")
 public class BaseApplication {
 
     public static void main(String[] args) {
