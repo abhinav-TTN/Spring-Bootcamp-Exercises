62945426c50f52ecfcf75653a44481bbf0745fd7
Abhinav Chaudhary <abhinav.chaudhary@tothenew.com>
2025-07-30 23:23:37 +0530

    Implemented ManyToMany mapping between Author and Book

--- src/main/java/com/abhinav/base/BaseApplication.java
+++ src/main/java/com/abhinav/base/BaseApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("com.abhinav.base.entityOneToManyBiDirection")
+@EntityScan("com.abhinav.base.entityManyToMany")
 public class BaseApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/abhinav/base/entityManyToMany/Book.java
@@ -0,0 +1,19 @@
+package com.abhinav.base.entityManyToMany;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Book {
+    @Id
+    private Long id;
+    private String bookName;
+
+    @ManyToMany(mappedBy = "book", cascade = CascadeType.ALL)
+    private List<Author> author;
+}
